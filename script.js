bdfbbfb6be2c80846d6f2c3ac4e2c6137e0b6529
lsnprt <115103372+lsnprt@users.noreply.github.com>
2023-04-24 19:16:03 +0200

    Implement responsive grid for any number

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++){
+const gridbase = 16;
+
+for(let i = 0; i < (gridbase * gridbase); i++){
     const pixel = document.createElement("div");
+    pixel.setAttribute("style", `min-width: ${100 / gridbase}%; min-height: ${100 / gridbase}%;`)
     container.appendChild(pixel);
 }
