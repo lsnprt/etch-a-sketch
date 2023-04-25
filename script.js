b5aec1ac513a595293e33670e87d20c210bfeb03
lsnprt <115103372+lsnprt@users.noreply.github.com>
2023-04-25 11:57:00 +0200

    Implement working shadow function

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-const container = document.querySelector(".container");
+window.addEventListener("load", initializeGrid(16));
 
-const gridbase = 16;
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    clearGrid();
+    const userPrompt = getUserPrompt();
+    initializeGrid(userPrompt);
+});
 
-for(let i = 0; i < (gridbase * gridbase); i++){
-    const pixel = document.createElement("div");
-    pixel.setAttribute("style", `min-width: ${100 / gridbase}%; min-height: ${100 / gridbase}%;`)
-    container.appendChild(pixel);
+function initializeGrid(gridbase) {
+    const container = document.querySelector(".container");
+    
+    for(let i = 0; i < (gridbase * gridbase); i++){
+        const pixel = document.createElement("div");
+        
+        pixel.setAttribute("class", "pixel");
+        pixel.setAttribute("style", `min-width: ${100 / gridbase}%; min-height: ${100 / gridbase}%;`);
+        container.appendChild(pixel);
+
+        pixel.addEventListener("mouseover", function(e) {
+             let backgroundProperty = window.getComputedStyle(this).backgroundColor;
+             let rgbArray = backgroundProperty.slice(4, -1).split(",").map(Number);
+             const rgbNewValue = rgbArray[0] - 25.5;
+             
+             if (rgbNewValue > 0){
+                 e.target.style.backgroundColor = `rgb(${rgbNewValue}, ${rgbNewValue}, ${rgbNewValue})`;
+             }
+         })
+
+    }
 }
+
+function getUserPrompt() {
+    let keepGoing = true;
+
+    while(keepGoing){
+        const userChoice = prompt("Select a number (0-100)");
+        const userChoiceParsed = Number.parseInt(userChoice)
+        
+        if (userChoiceParsed > 0 && userChoiceParsed <= 100){
+            return userChoiceParsed;
+        } 
+    }
+}
+
+function clearGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    const container = document.querySelector(".container");
+    
+    for(let i = pixels.length; i > 0; i--){
+        container.removeChild(container.lastChild);
+    }
+}
+
